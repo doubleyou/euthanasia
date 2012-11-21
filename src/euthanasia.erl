ee8cfce0649ef5fde5e486b1685c7edb18530427
Dmitry Demeshchuk <demeshchuk@gmail.com>
2012-11-21 06:30:54 +0400

    Added euthanasia:register/0

--- src/euthanasia.erl
+++ src/euthanasia.erl
@@ -3,10 +3,13 @@
 -compile({no_auto_import,[register/2, unregister/1]}).
 
 -export([start/0]).
--export([register/1, register/2, unregister/1]).
+-export([register/0, register/1, register/2, unregister/1]).
 
 start() ->
     application:start(euthanasia).
+
+register() ->
+    register(self()).
 
 register(Pid) ->
     register(Pid, []).
